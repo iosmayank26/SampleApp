4abe17bee52a54bdce2c3a4e1ef181eb0c06f0bb
Anil Kumar Giri <admin@Anils-Mac-mini.local>
2017-09-13 15:56:48 +0530

    Adding whatever

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var xyz = "vfhjvjhbgjk"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
